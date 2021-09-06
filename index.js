858589d156b0c27aa1411c73256c7863913f89bb
Abhishek Vaish <abhishek.vaish_int18@gla.ac.in>
2021-09-06 17:41:46 +0530

    add cors middleware

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
 require("dotenv").config();
+
 const userRoute = require("./routes/user");
+
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 try {
   mongoose.connect(
     process.env.MONGODB_URI,
